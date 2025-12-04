2adc92fbb68654e95e8d279d75aa14ee53667c10
Roman Tuomisto <romantuomisto@gmail.com>
2025-12-04 20:44:02 +0200

    completes character card and form

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Card from '@/components/Card/card-element.vue'
+import CardHeader from '@/components/Card/card-header.vue'
+import CardStats from '@/components/Card/card-stats.vue'
+import EditForm from '@/components/Form/edit-form.vue'
+
+//starter data
+const data = {
+  name: 'Flora',
+  level: 100,
+  job: 'sorcerer',
+  race: 'human',
+  bio: 'Flora is an Exclusive Legendary Wild Hero from the Fire element, known for her powerful single-target damage and her pet dragon. With high attack and enlightenment stats, Flora excels when paired with multi-hitting Wild allies, making her the queen of precise, devastating strikes. Ideal companions include the Epic Hero Tonalnan and Legendaries such as Felicity and Errich, who complement her abilities perfectly. To maximize Flora’s potential, it is crucial to time her skills effectively, ensuring that her Ultimate skill is used first, followed by the ultimates of her Wild allies. This strategy allows her dragon to unleash a torrent of incredibly powerful damage on enemies, securing Flora’s place as a top-tier hero in any Wild team. Her unique combination of strength and synergy makes her an indispensable asset for players aiming to dominate their foes with fiery precision.',
+}
+
+//data
+const name = ref(data.name)
+const level = ref(data.level)
+const job = ref(data.job)
+const race = ref(data.race)
+const bio = ref(data.bio)
+
+//respawn function
+const respawn = () => {
+  name.value = data.name
+  level.value = data.level
+  job.value = data.job
+  race.value = data.race
+  bio.value = data.bio
+}
+</script>
+
+<template>
+  <main>
+    <section>
+      <Card v-bind:respawn="respawn">
+        <CardHeader :name="name" :level="level" :job="job" />
+        <CardStats :race="race" :bio="bio" />
+      </Card>
+    </section>
+    <section>
+      <EditForm
+        v-model:name.trim.capitalize_first_letter="name"
+        v-model:level.number="level"
+        v-model:job="job"
+        v-model:race="race"
+        v-model:bio="bio"
+      />
+    </section>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 50px;
+  padding: 50px 20px;
+}
+section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Card/card-element.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps<{
+  //respawn function
+  respawn: () => void
+}>()
+</script>
+
+<template>
+  <article class="card-element">
+    <slot />
+    <button @click="respawn">Respawn?</button>
+  </article>
+</template>
+
+<style scoped>
+.card-element {
+  background: white;
+  padding: 25px;
+  max-width: 500px;
+  transform: rotate(1deg);
+  box-shadow: #00000022 5px 5px 2px;
+  border: double #a68d67 3px;
+  margin-bottom: 70px;
+  padding-bottom: 30px;
+}
+.card-element button {
+  background-color: #a68d67;
+  border: 0;
+  color: white;
+  padding: 5px 15px;
+  font-weight: bold;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  font-size: 0.9rem;
+  cursor: pointer;
+  transition: transform 0.2s;
+}
+.card-element button:hover {
+  transform: scale(1.1);
+}
+</style>
